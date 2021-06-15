8a73ee79e7f9842e6399c7c09096c51afa0eb9d6
Rafael R Doimo <rafrodrigo@gmail.com>
2021-06-15 10:11:11 -0300

    Create determinante.py

---
+++ determinante.py
@@ -0,0 +1,24 @@
+'''Programa que irá calcular o determinante de uma matriz quadrada de ordem mxn'''
+print('\033[34m~'*40)
+print('DETERMINANTE DE UMA MATRIZ:'.center(40))
+print('~'*40, '\033[m')
+matriz = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+m = int(input('qual a ordem de sua matriz: '))
+for l in range(0, m):
+    for c in range(0, m):
+        matriz[l][c] = int(input((f'Digite o valor {l+1}X{c+1}: ')))
+print('\033[34m~'*40, '\033[m')
+for l in range(0, m):
+    for c in range(0, m):
+            print(f'{matriz[l][c]:^6}', end='')
+    print()
+#calculo do determinandte
+#multiplicando as diagonais
+'''i = j = 0
+for l in range(0, m):
+    for c in range(0, m):'''
+
+
